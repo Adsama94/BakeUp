62bd93ce38df5082a99cefa6b87a2676177ca077
PARAN SHARMA <paransharma94@gmail.com>
2017-10-06 00:14:20 +0530

    Added checkboxes for recipe selection

--- app/src/main/java/com/adsama/android/bakeup/Widget/RecipeWidgetSelector.java
+++ app/src/main/java/com/adsama/android/bakeup/Widget/RecipeWidgetSelector.java
@@ -2,9 +2,11 @@ package com.adsama.android.bakeup.Widget;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.RadioButton;
 
 import com.adsama.android.bakeup.R;
+import com.vstechlab.easyfonts.EasyFonts;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,5 +27,41 @@ public class RecipeWidgetSelector extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recipe_selector);
         ButterKnife.bind(this);
+        mRadioPie.setTypeface(EasyFonts.droidSerifBold(this));
+        mRadioBrownie.setTypeface(EasyFonts.droidSerifBold(this));
+        mRadioYellow.setTypeface(EasyFonts.droidSerifBold(this));
+        mRadioCheese.setTypeface(EasyFonts.droidSerifBold(this));
+        mRadioPie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRadioBrownie.setChecked(false);
+                mRadioYellow.setChecked(false);
+                mRadioCheese.setChecked(false);
+            }
+        });
+        mRadioBrownie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRadioPie.setChecked(false);
+                mRadioYellow.setChecked(false);
+                mRadioCheese.setChecked(false);
+            }
+        });
+        mRadioYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRadioPie.setChecked(false);
+                mRadioBrownie.setChecked(false);
+                mRadioCheese.setChecked(false);
+            }
+        });
+        mRadioCheese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRadioPie.setChecked(false);
+                mRadioBrownie.setChecked(false);
+                mRadioYellow.setChecked(false);
+            }
+        });
     }
 }
